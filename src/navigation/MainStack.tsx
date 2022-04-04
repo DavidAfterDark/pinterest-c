a1abbeb843e04840d3a6369afe1a6fcecb758441
HankTech <hanktech1.0@gmail.com>
2022-04-03 22:51:41 -0400

    add/icons for profile screen

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { View, Platform } from 'react-native'
 import { useTheme } from '@react-navigation/native'
 import { HeaderBackground } from '@react-navigation/elements'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
@@ -12,6 +13,9 @@ import Profile from '../screens/Profile'
 //  icons
 import IconHome from '../components/Svg/IconHome'
 import IconProfile from '../components/Svg/IconProfile'
+import IconShareAndroid from '../components/Svg/IconShareAndroid'
+import IconShareIos from '../components/Svg/IconShareIos'
+import IconEllipsis from '../components/Svg/IconEllipsis'
 
 const Tab = createBottomTabNavigator()
 const Stack = createNativeStackNavigator()
@@ -46,8 +50,8 @@ const MainTabs = () => {
   return (
      <Tab.Navigator screenOptions={{
        tabBarShowLabel: false,
-       tabBarActiveTintColor: theme.dark ? 'gray' : 'black',
-       tabBarInactiveTintColor: theme.dark ? '#4f4d4d' : 'black'
+       tabBarActiveTintColor: theme.dark ? '#fff' : '#000',
+       tabBarInactiveTintColor: theme.dark ? '#4f4d4d' : 'gray'
      }}>
       <Tab.Screen
         name='HomeScreen'
@@ -62,7 +66,12 @@ const MainTabs = () => {
         component={Profile}
         options={() => ({
           title: '',
-          headerRight: () => null,
+          headerRight: () => (
+            <View style={{ flexDirection: 'row' }}>
+              {Platform.OS === 'android' ? <IconShareAndroid color={theme.colors.text} size={30} /> : <IconShareIos color={theme.colors.text} size={30} />}
+              <View style={{ marginLeft: 10, marginRight: 5 }}><IconEllipsis color={theme.dark ? 'white' : 'black' } size={30} /></View>
+            </View>
+          ),
           headerBackground: () => <HeaderBackground style={{ backgroundColor: theme.colors.background }} />,
           headerShadowVisible: false,
           tabBarIcon: ({ color }) => <IconProfile color={color} size={35} />
