cb1beae1134067ef28d14e5ea8abe1887d7e5767
HankTech <HankTech@gmail.com>
2022-04-06 14:32:36 -0400

    add/ModalButton

---
+++ src/components/Svg/IconPlus.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Svg, { Path } from 'react-native-svg'
+import { IconSvgProps } from '../../types/svgProps'
+
+const IconPlus = ({ size, color }: IconSvgProps) => (
+  <Svg
+    viewBox="0 0 448 512"
+    width={size || 24}
+    height={size || 24}
+    fill={color}
+  >
+    <Path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99h144v-144C192 62.32 206.33 48 224 48s32 14.32 32 32.01v144h144c17.7-.01 32 14.29 32 31.99z" />
+  </Svg>
+)
+
+export default IconPlus
--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { View, Platform } from 'react-native'
-import { useTheme } from '@react-navigation/native'
-import { HeaderBackground } from '@react-navigation/elements'
+import React, { useContext } from 'react'
+import { View, Platform, Pressable } from 'react-native'
+import { useTheme, NavigatorScreenParams } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack'
+import ModalAddContext from '../context/ModalAdd/ModalAddContext'
 
 //  screens
 import Home from '../screens/Home'
@@ -16,13 +16,33 @@ import IconProfile from '../components/Svg/IconProfile'
 import IconShareAndroid from '../components/Svg/IconShareAndroid'
 import IconShareIos from '../components/Svg/IconShareIos'
 import IconEllipsis from '../components/Svg/IconEllipsis'
+import IconPlus from '../components/Svg/IconPlus'
 
-const Tab = createBottomTabNavigator()
-const Stack = createNativeStackNavigator()
+type MainStackParamList = {
+  Home: undefined;
+  Pin: { id: string }
+}
+
+type TabParamList = {
+  HomeScreen: NavigatorScreenParams<MainStackParamList>;
+  ButtonModal: undefined;
+  Profile: { userId: string };
+};
+
+const Stack = createNativeStackNavigator<MainStackParamList>()
+
+const Tab = createBottomTabNavigator<TabParamList>()
+
+type Props = NativeStackScreenProps<MainStackParamList, 'Pin'>
 
 const MainStack = () => {
   return (
-    <Stack.Navigator screenOptions={{ animation: 'slide_from_right' }}>
+    <Stack.Navigator
+      screenOptions={{
+        animation: 'slide_from_right',
+        contentStyle: { backgroundColor: 'transparent' }
+      }}
+    >
         <Stack.Screen
           name='Home'
           component={MainTabs}
@@ -47,12 +67,15 @@ const MainStack = () => {
 const MainTabs = () => {
   const theme = useTheme()
 
+  const { setVisible } = useContext(ModalAddContext)
+
   return (
-     <Tab.Navigator screenOptions={{
-       tabBarShowLabel: false,
-       tabBarActiveTintColor: theme.dark ? '#fff' : '#000',
-       tabBarInactiveTintColor: theme.dark ? '#4f4d4d' : 'gray'
-     }}>
+     <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: theme.dark ? '#fff' : '#000',
+        tabBarInactiveTintColor: theme.dark ? '#4f4d4d' : 'gray'
+      }}>
       <Tab.Screen
         name='HomeScreen'
         component={Home}
@@ -60,6 +83,37 @@ const MainTabs = () => {
           header: () => null,
           tabBarIcon: ({ color }) => <IconHome color={color} size={35} />
         })} />
+
+        <Tab.Screen
+          name='ButtonModal'
+          component={ButtonModal}
+          options={{
+            tabBarButton: () => (
+              <Pressable
+                style={{
+                  backgroundColor: theme.dark ? '#3D3D3Dff' : '#EAEAEC',
+                  width: 45,
+                  height: 45,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: 50,
+                  top: -10
+                }}
+                onPress={() => setVisible(true)}
+              >
+                <IconPlus size={30} color={theme.dark ? '#fff' : '#000'} />
+              </Pressable>
+            )
+          }}
+          listeners={() => {
+            return {
+              tabPress: (e) => {
+                e.preventDefault()
+                setVisible(true)
+              }
+            }
+          }}
+        />
 
       <Tab.Screen
         name='Profile'
@@ -72,13 +126,17 @@ const MainTabs = () => {
               <View style={{ marginLeft: 10, marginRight: 5 }}><IconEllipsis color={theme.dark ? 'white' : 'black' } size={30} /></View>
             </View>
           ),
-          headerBackground: () => <HeaderBackground style={{ backgroundColor: theme.colors.background }} />,
           headerShadowVisible: false,
-          tabBarIcon: ({ color }) => <IconProfile color={color} size={35} />
+          tabBarIcon: ({ color }) => <IconProfile color={color} size={35} />,
+          headerTransparent: true
         })}
       />
     </Tab.Navigator>
   )
+}
+
+const ButtonModal = () => {
+  return <View />
 }
 
 export default MainStack
