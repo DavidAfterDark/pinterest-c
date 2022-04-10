a259d732080fdbd1a076f599cc20d7d381ebe019
HankTech <hanktech1.0@gmail.com>
2022-04-10 11:23:47 -0400

    add/icons

---
+++ src/components/Svg/IconChevronDown.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Svg, { Path } from 'react-native-svg'
+import { IconSvgProps } from '../../types/SvgProps'
+
+const IconChevronDown = ({ size, color }: IconSvgProps) => (
+  <Svg
+    viewBox="0 0 384 512"
+    width={size || 24}
+    height={size || 24}
+    fill={color || 'red'}
+  >
+    <Path d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z" />
+  </Svg>
+)
+
+export default IconChevronDown
