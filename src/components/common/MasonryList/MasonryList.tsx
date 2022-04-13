1bfc0922946c6cbf0d84229b7352d322f92bc389
HankTech <HankTech@gmail.com>
2022-04-13 11:48:50 -0400

    dynamic cols for masonry list

--- src/components/common/MasonryList/MasonryList.tsx
+++ src/components/common/MasonryList/MasonryList.tsx
@@ -1,4 +1,4 @@
-import { View, ScrollView, StyleSheet } from 'react-native'
+import { View, ScrollView, StyleSheet, useWindowDimensions } from 'react-native'
 import React from 'react'
 
 //  components
@@ -11,16 +11,18 @@ interface MasonryListProps {
 }
 
 const MasonryList = ({ data, favoritesButton = false, onPressFavoriteButton }: MasonryListProps) => {
+  const width = useWindowDimensions().width
+
+  const numColumns = Math.ceil(width / 300)
+
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <View style={styles.container}>
-        <View style={styles.column}>
-          {data.filter((_item, index) => index % 2 === 0).map((pin) => <Pin key={pin.id} pin={pin} favoritesButton={favoritesButton} onPressFavoriteButton={onPressFavoriteButton} />)}
-        </View>
-
-        <View style={styles.column}>
-          {data.filter((_item, index) => index % 2 === 1).map((pin) => <Pin key={pin.id} pin={pin} favoritesButton={favoritesButton} onPressFavoriteButton={onPressFavoriteButton} />)}
-        </View>
+        {Array.from(Array(numColumns)).map((col, colIndex) => (
+          <View style={styles.column} key={colIndex}>
+            {data.filter((_item, index) => index % numColumns === colIndex).map((pin) => <Pin key={pin.id} pin={pin} favoritesButton={favoritesButton} onPressFavoriteButton={onPressFavoriteButton} />)}
+          </View>
+        ))}
       </View>
     </ScrollView>
   )
