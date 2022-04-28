3a0c8c9f5c55dd8b06120c6be6658d7d27d57267
HankTech <HankTech@gmail.com>
2022-04-28 16:21:11 -0400

    email regex

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,1 +1,5 @@
+//  backend url
 export const BACKEND_URL = 'https://pmnlxompbegobtposrzs.nhost.run'
+
+//  regexs
+export const EMAIL_REGEX = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
