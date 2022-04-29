2865efd006c0f3bee11450c532d375bf29064f81
HankTech <HankTech@gmail.com>
2022-04-29 10:22:28 -0400

    add icon and props

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -1,15 +1,45 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Intro from '../screens/Auth/Intro'
+import { AuthStackParamList } from '../types/NavigationProps'
 
-const Stack = createNativeStackNavigator()
+//  screens
+import Intro from '../screens/Auth/Intro'
+import SignInScreen from '../screens/Auth/SignInScreen'
+import SignUpScreen from '../screens/Auth/SignUpScreen'
+
+//  icons
+import IconXMark from '../components/Svg/IconXMark'
+import { Pressable } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+const Stack = createNativeStackNavigator<AuthStackParamList>()
 
 const AuthStack = () => {
+  const navigation = useNavigation()
+
   return (
     <Stack.Navigator>
       <Stack.Screen
         name='IntroScreen'
         component={Intro}
+        options={{ headerShown: false }}
+      />
+
+      <Stack.Screen
+        name='SignInScreen'
+        component={SignInScreen}
+        options={{
+          title: 'Iniciar sesión',
+          headerTitleStyle: { fontSize: 18 },
+          headerTitleAlign: 'center',
+          headerBackVisible: false,
+          headerLeft: () => <Pressable style={{ padding: 3 }} onPress={() => navigation.goBack()}><IconXMark color='#6D6D6D' size={20} /></Pressable>
+        }}
+      />
+
+      <Stack.Screen
+        name='SignUpScreen'
+        component={SignUpScreen}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
