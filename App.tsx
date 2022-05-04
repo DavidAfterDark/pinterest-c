1b855da18fa5fc49f6e657e5400a428f1e41745f
HankTech <HankTech@gmail.com>
2022-05-04 10:31:27 -0400

    delete nHost

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import Root from './src/navigation'
 import BottomModalState from './src/context/BottomModal/BottomModalState'
-import { NhostClient, NhostReactProvider } from '@nhost/react'
-import { BACKEND_URL } from './src/constant'
-
-const nhost = new NhostClient({
-  backendUrl: BACKEND_URL
-})
 
 const App = () => {
   return (
-    <NhostReactProvider nhost={nhost}>
       <BottomModalState>
         <Root />
       </BottomModalState>
-    </NhostReactProvider>
   )
 }
 
