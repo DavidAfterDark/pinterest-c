bb1544a858581a634325404e1349ea8fa327547d
HankTech <HankTech@gmail.com>
2022-05-16 13:41:56 -0400

    add NHost

--- App.tsx
+++ App.tsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import Root from './src/navigation'
 import BottomModalState from './src/context/BottomModal/BottomModalState'
-// import { Amplify } from 'aws-amplify'
-// import awsconfig from './src/aws-exports'
 import { QueryClient, QueryClientProvider } from 'react-query'
-
-// Amplify.configure(awsconfig)
+import { NhostClient, NhostReactProvider } from '@nhost/react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const queryClient = new QueryClient()
 
+const nhost = new NhostClient({
+  backendUrl: 'https://hdprcyrkfjmxrdcwumfn.nhost.run',
+  clientStorageType: 'react-native',
+  clientStorage: AsyncStorage
+})
+
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BottomModalState>
-        <Root />
-      </BottomModalState>
-    </QueryClientProvider>
+    <NhostReactProvider nhost={nhost}>
+      <QueryClientProvider client={queryClient}>
+        <BottomModalState>
+          <Root />
+        </BottomModalState>
+      </QueryClientProvider>
+    </NhostReactProvider>
   )
 }
 
